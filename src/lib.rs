13e3af7dce0f1a7706a2f3a459f40ed259ef9c18
xxai.art <xxai.art@gmail.com>
2023-08-17 16:35:23 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[macro_export]
+macro_rules! q {
+  ($db:ident, $name:ident) => {
+    lazy_static::lazy_static! {
+      static ref $db: Pg = Pg::new_with_env(format!("{}_URI",stringify!($db)));
     }
+    paste! {
+      q!($db, $name, query, Vec<Row>);
+      q!($db, [<$name 1>], query_one, Row);
+      q!($db, [<$name 01>], query_opt, Option<Row>);
+    }
+  };
+  ($db:ident,$name:ident,$func:ident,$rt:ty) => {
+    #[allow(non_snake_case)]
+    pub async fn $name<T: ToStatement>(
+      statement: impl IntoStatement<T>,
+      params: &[&(dyn ToSql + Sync)],
+    ) -> Result<$rt, Error> {
+      $db.$func(statement, params).await
+    }
+  };
 }
