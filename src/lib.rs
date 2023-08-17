39b08eefb81e65a85110db63357447ef11d4f182
xxai.art <xxai.art@gmail.com>
2023-08-17 16:49:16 +0800

    ♨

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
+pub use lazy_static::lazy_static;
+pub use paste::paste;
+pub use pgw::{Error, IntoStatement, Pg, Row, ToSql, ToStatement};
+
 #[macro_export]
 macro_rules! q {
   ($db:ident, $name:ident) => {
-    lazy_static::lazy_static! {
-      static ref $db: Pg = Pg::new_with_env(format!("{}_URI",stringify!($db)));
+    $crate::lazy_static! {
+      static ref $db: $crate::Pg = $crate::Pg::new_with_env(format!("{}_URI",stringify!($db)));
     }
-    paste! {
-      q!($db, $name, query, Vec<Row>);
-      q!($db, [<$name 1>], query_one, Row);
-      q!($db, [<$name 01>], query_opt, Option<Row>);
+    $crate::paste! {
+      q!($db, $name, query, Vec<$crate::Row>);
+      q!($db, [<$name 1>], query_one, $crate::Row);
+      q!($db, [<$name 01>], query_opt, Option<$crate::Row>);
     }
   };
   ($db:ident,$name:ident,$func:ident,$rt:ty) => {
     #[allow(non_snake_case)]
-    pub async fn $name<T: ToStatement>(
-      statement: impl IntoStatement<T>,
-      params: &[&(dyn ToSql + Sync)],
-    ) -> Result<$rt, Error> {
-      $db.$func(statement, params).await
+    pub async fn $name<T: $crate::ToStatement>(
+      statement: impl $crate::IntoStatement<T>,
+      params: &[&(dyn $crate::ToSql + Sync)],
+    ) -> Result<$rt, $crate::Error> {
+      (*$db).$func(statement, params).await
     }
   };
 }
